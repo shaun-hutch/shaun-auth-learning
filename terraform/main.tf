eb148cf7bdcf0a8cfd124f7af76f8bff4697dc3b
Shaun <shutchnz@gmail.com>
2025-09-26 22:00:43 +1200

    Remove output for public IP address of the EC2 instance from Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-# Output the public IP address of the EC2 instance
-output "instance_public_ip" {
-  description = "Public IP address of the API EC2 instance"
-  value       = aws_instance.api.public_ip
-}
 terraform {
   required_providers {
     aws = {
